0d565c1c609827e5586bbd1eb682207f3e18ac7a
msayracode <msayracode@gmail.com>
2025-12-16 10:19:25 +0300

    movies app

--- catalog/catalog/settings.py
+++ catalog/catalog/settings.py
@@ -32,7 +32,8 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
-    'pages.apps.PagesConfig',
+    'movies.apps.MoviesConfig',
+    'pages',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
