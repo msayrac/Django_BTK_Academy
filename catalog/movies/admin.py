e4c03c077b918fc30d1b4ceef05299fb65bad3c4
msayracode <msayracode@gmail.com>
2025-12-18 12:52:21 +0300

    admin panel properties

--- catalog/movies/admin.py
+++ catalog/movies/admin.py
@@ -2,4 +2,19 @@ from django.contrib import admin
 from .models import Movie
 # Register your models here.
 
-admin.site.register(Movie)
+class MovieAdmin(admin.ModelAdmin):
+    list_display = ("id","name","created_date","isPublished")
+    list_display_links = ("id","name","created_date")
+    list_filter = ("created_date","name")
+    list_editable =("isPublished",)
+    search_fields = ("name","description")
+    list_per_page = 20
+
+
+
+admin.site.register(Movie,MovieAdmin)
+
+
+
+
+
--- catalog/movies/models.py
+++ catalog/movies/models.py
@@ -2,9 +2,13 @@ from django.db import models
 
 # Create your models here.
 class Movie(models.Model):
-    name = models.CharField(max_length=100)
-    description = models.TextField()
-    image =models.CharField(max_length=50)
-    created_date = models.DateTimeField(auto_now_add=True)
+    name = models.CharField(max_length=100, verbose_name="Film Adı")
+    description = models.TextField(verbose_name="Film Açıklaması")
+    image =models.CharField(max_length=50,verbose_name="Film Görseli")
+    created_date = models.DateTimeField(auto_now_add=True,verbose_name="Eklenme Tarihi")
+    isPublished = models.BooleanField(default = True)
+    
+    def __str__(self):
+        return self.name
 
 
